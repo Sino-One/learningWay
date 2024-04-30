616bd9f73ea7d4610670353ede0173b8aaf4db0e
Sinwan Meynaud <sinwan.meynaud@gmail.com>
2024-04-30 09:08:55 +0200

    fix some ft

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -11,9 +11,63 @@ import { Button } from "./button";
 import { useFormState, useFormStatus } from "react-dom";
 import { authenticate } from "@/app/lib/actions";
 import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { signIn } from "next-auth/react";
 
 export default function LoginForm() {
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [emailInPutError, setEmailInputError] = useState(false);
+  const [passwordInPutError, setPasswordInputError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    validate();
+  }, [email, password]);
+
+  const validateEmail = (email: string) => {
+    let re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    let res = await signIn("credentials", {
+      email,
+      password,
+      callbackUrl: `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}`,
+      redirect: false,
+    });
+
+    if (res?.ok) {
+      // toast success
+      console.log("success");
+      return;
+    } else {
+      // Toast failed
+      setError("Failed! Check you input and try again.");
+      // return;
+      console.log("Failed", res);
+    }
+    return res;
+  }
+
+  function validate() {
+    let emailIsValid = validateEmail(email);
+
+    if (!emailIsValid) {
+      setEmailInputError(true);
+      return;
+    }
+    if (password.length < 6) {
+      setPasswordInputError(true);
+    } else {
+      setEmailInputError(false);
+      setPasswordInputError(false);
+    }
+  }
 
   return (
     <form action={dispatch} className="space-y-3">
--- app/ui/signup-form.tsx
+++ app/ui/signup-form.tsx
@@ -5,23 +5,88 @@ import {
   AtSymbolIcon,
   KeyIcon,
   ExclamationCircleIcon,
+  UserIcon,
 } from "@heroicons/react/24/outline";
 import { ArrowRightIcon } from "@heroicons/react/20/solid";
 import { Button } from "./button";
 import { useFormState, useFormStatus } from "react-dom";
 import { authenticate } from "@/app/lib/actions";
 import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 export default function SignUpForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passError, setPassError] = useState(false);
+
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
 
+  useEffect(() => {
+    validatePassword(password, confirmPassword);
+  }, [password, confirmPassword]);
+
+  function validatePassword(pass: String, confirmPass: String) {
+    let isValid = confirmPass === pass;
+    if (!isValid) {
+      setPassError(true);
+    }
+  }
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    let userData = {
+      name,
+      email,
+      password,
+    };
+
+    // Make call to backend to create user
+    const res = await fetch("http://localhost:3000/api/user/create", {
+      method: "POST",
+      body: JSON.stringify(userData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (res.ok) {
+      const data = await res.json();
+
+      // registration success
+    } else {
+      //registration faled
+    }
+  }
   return (
-    <form action={dispatch} className="space-y-3">
+    <form onSubmit={handleSubmit} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please sign up to continue.
         </h1>
         <div className="w-full">
+          <div>
+            <label
+              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              htmlFor="name"
+            >
+              Name
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="name"
+                type="name"
+                name="name"
+                placeholder="Enter your Name"
+                required
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+              <UserIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+          </div>
           <div>
             <label
               className="mb-3 mt-5 block text-xs font-medium text-gray-900"
@@ -37,6 +102,9 @@ export default function SignUpForm() {
                 name="email"
                 placeholder="Enter your email address"
                 required
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
@@ -57,24 +125,30 @@ export default function SignUpForm() {
                 placeholder="Enter password"
                 required
                 minLength={6}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
               />
               <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
             <label
               className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="password"
+              htmlFor="confirm-password"
             >
               Confirm password
             </label>
             <div className="relative">
               <input
                 className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="password"
+                id="confirm-password"
                 type="password"
-                name="password"
-                placeholder="Enter password"
+                name="confirm-password"
+                placeholder="Enter password confirmation"
                 required
                 minLength={6}
+                onChange={(e) => {
+                  setConfirmPassword(e.target.value);
+                }}
               />
               <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
